8d010e5d5a860d28a9a8b574cca309e5f8645136
Self-denial <731813856@qq.com>
2016-07-23 19:39:56 +0800

    values

---
+++ app/src/main/java/duanone/one/com/duanone/MainActivity.java
@@ -0,0 +1,13 @@
+package duanone.one.com.duanone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
